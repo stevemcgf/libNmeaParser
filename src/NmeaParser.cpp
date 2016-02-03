9fde9a2b847665f57b14c3e7e0e12dc3571627f6
Steve Mc Gregor <steve@mcgregor.pe>
2016-02-03 14:00:22 -0500

    Se agrega proyectos ConfigManager y libNmeaParser

---
+++ src/NmeaParser.cpp
@@ -0,0 +1,18 @@
+/*
+ * NmeaParser.cpp
+ *
+ *  Created on: Feb 3, 2016
+ *      Author: steve
+ */
+
+#include "NmeaParser.h"
+
+NmeaParser::NmeaParser() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NmeaParser::~NmeaParser() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/NmeaParser.h
@@ -0,0 +1,17 @@
+/*
+ * NmeaParser.h
+ *
+ *  Created on: Feb 3, 2016
+ *      Author: steve
+ */
+
+#ifndef SRC_NMEAPARSER_H_
+#define SRC_NMEAPARSER_H_
+
+class NmeaParser {
+public:
+	NmeaParser();
+	virtual ~NmeaParser();
+};
+
+#endif /* SRC_NMEAPARSER_H_ */
